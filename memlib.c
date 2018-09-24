22d57e6e13d6de7ba8f5a910d5aad5a6bddd95d4
Dustin Fast <dustin.fast@hotmail.com>
2018-09-23 21:45:35 -0800

    Fixed global init issue -DF

--- memlib.c
+++ memlib.c
@@ -3,35 +3,32 @@
 // Author: Dustin Fast
  
 #include <stdio.h>  // debug
+#include <stdlib.h>
 #include <stddef.h>
 #include <sys/mman.h>
 
 /* BEGIN Definitions ------------------------------------------------------ */
 
-/*  Here we define our heap & memory-block elements, which are ultimately
-    structured as follows:
+/*  Here we define our heap & memory-block elements, which coalesce to
+    represent the Heap and Memory Block structures, like so:
 
      Memory Block        Memory Block            Heap
     (Unallocated)        (Allocated)        --------------
     --------------      --------------      |  HeapInfo  |      
     | BlockInfo  |      | BlockInfo  |      --------------  
     --------------      --------------      |     ...    |  
-    |   (free    |      |    ...     |      | (allocated |
-    |    space)  |      |   (block   |      |   blocks)  |
+    |   (free    |      |    ...     |      |     ...    |
+    |    space)  |      |   (block   |      |   (blocks) |
     --------------      |    data)   |      |     ...    |
     | BlockInfo  |      |    ...     |      |     ...    |
     --------------      --------------      --------------
 
-    Note: We don't actually define our heap or memory blocks anywhere,
-          they exist "notionally" in memory. 
     Note: Allocated memory blocks don't get a BlockInfo footer - the footer
           is only used as a helper to combine unused blocks of memory.
     Note: Calls to our malloc, etc., functions return ptrs to the block
           data inside the memory block, not to the memory block itself.
 */
 
-// Global heap ptr
-static void *g_heap = NULL;
 
 // Struct BlockInfo - Information about a memory block
 typedef struct BlockInfo {
@@ -45,13 +42,16 @@ typedef struct BlockInfo {
 
 // Struct HeapInfo - Information about the memory heap.
 typedef struct HeapInfo {
-    BlockInfo *first_free;    // Ptr to head of the "free memory" list 
-    char *start_addr;       // Ptr to first byte of heap
-    char *data_addr;        // Ptr to start address of heap's mem blocks.
-    char *end_addr;         // Ptr to last byte of heap
-    char *max_addr;         // Ptr to max legal heap address
-    size_t size;            // Denotes total size of the heap
+    BlockInfo *first_free;      // Ptr to head of the "free memory" list 
+    char *start_addr;           // Ptr to first byte of heap
+    char *data_addr;            // Ptr to start address of heap's mem blocks.
+    char *end_addr;             // Ptr to last byte of heap
+    char *max_addr;             // Ptr to max legal heap address
+    size_t size;                // Denotes total size of the heap
 } HeapInfo;
+
+// Global heap ptr
+static HeapInfo *g_heap = NULL;
 
 // Denote size of the above structs. Will be 48 bytes each on 64-bit machines.
 #define BLOCK_INFO_SZ sizeof(BlockInfo)
@@ -61,43 +61,81 @@ typedef struct HeapInfo {
 #define WORD_SZ sizeof(void*)   // 8 bytes, on a 64-bit machine
 
 // Denote minumum memory block sizes
-#define MIN_USED_BLOCK_SZ (BLOCK_INFO_SZ + WORD_SZ)
-#define MIN_FREE_BLOCK_SZ (BLOCK_INFO_SZ + WORD_SZ + BLOCK_INFO_SZ)
+#define MIN_BLOCK_SZ (BLOCK_INFO_SZ + WORD_SZ + BLOCK_INFO_SZ)
 
 // Denote min size of the heap as one free block plus the HeapInfo header
-#define MIN_HEAP_SZ (HEAP_INFO_SZ + MIN_FREE_BLOCK_SZ)
+#define MIN_HEAP_SZ (HEAP_INFO_SZ + MIN_BLOCK_SZ)
 
 // Define initial heap size, in MB.
-#define START_HEAP_SIZE 20
+#define START_HEAP_SZ 20
 
-// Define heap alignment = 8 bytes, making it 64-bit word-addressable, like so:
+// Define heap alignment, making it word-addressable, like so:
 //    -----------------------
 //    |   |   |   |  . . .  |
 //    -----------------------
 //    ^   ^   ^   ^         
 //    0   8   16  24
-#define HEAP_ALIGN 8
+#define HEAP_ALIGN WORD_SZ
 
 /* END Definitions -------------------------------------------------------- */
-/* Begin Helpers ---------------------------------------------------------- */
+/* Begin Linked List Helpers ---------------------------------------------- */
 
-// Increments the "used" portion of the heap by adding "add_bytes" to it.
+// Searches for a mem block >= "size" bytes in the given heap's "free" list.
+// Returns: A ptr to the found block, or NULL if none found.
+void *get_free_block(size_t size) {
+    BlockInfo* curr_block = g_heap->first_free;
+
+    while (curr_block) {
+        if (curr_block->size >= size)
+            return curr_block;
+        curr_block = curr_block->next;
+    }
+    return NULL;
+}
+
+// "Allocates" a block by removing it from the heap's "free" list.
+void allocate_block(BlockInfo *block) {
+    // BlockInfo *next, *prev;
+    // next = block->next;
+
+    // while (curr_block) {
+    //     if (curr_block->size >= size)
+    //         return curr_block;
+    //     curr_block = curr_block->next;
+    // }
+
+    // // If at EOL, patch its prev pointer.
+    // if (next)
+    //     next->prev = prev;
+
+    // // If we're removing the head of the free list, set the head to be
+    // // the next block, otherwise patch the previous block's next pointer.
+    // if (block == FREE_LIST_HEAD)
+    //     FREE_LIST_HEAD = next;
+    // else
+    //     prev->next = next;
+}
+
+/* End Linked List Helpers ------------------------------------------------ */
+/* Begin Mem Helpers ------------------------------------------------------ */
+
+// Increments the "used" portion of the given heap by adding "add_bytes" to it.
 // Returns: Ptr to the first byte of the newly added space, or NULL on fail.
-// void *incr_heap(size_t add_bytes) 
-// {
-//     char *old_end = heap.end;
+void *incr_heap(HeapInfo *heap, size_t add_bytes) 
+{
+    char *old_end = heap->end_addr;
 
-//     if (((heap.end + add_bytes) > heap.mem_max_addr)) {
-//         fprintf(stderr, "ERROR: Could not incremement heap - out of space.\n");  // debug
-//         return NULL;
-//     }
-//     heap.end += add_bytes;
-//     return old_end;
-// }
+    if (((heap->end_addr + add_bytes) > heap->max_addr)) {
+        fprintf(stderr, "ERROR: Could not incremement heap - out of space.\n");  // debug
+        return NULL;
+    }
+    heap->end_addr += add_bytes;
+    return old_end;
+}
 
 // Allocates a new mem space of size "length" using the mmap syscall.
 // Returns: A ptr to the mapped address space, or NULL on fail.
-void *do_mem_map(size_t length) {
+void *do_mmap(size_t length) {
     int prot = PROT_EXEC | PROT_READ | PROT_WRITE;
     int flags = MAP_PRIVATE | MAP_ANONYMOUS;
     void *result = mmap(NULL, length, prot, flags, -1, 0);
@@ -110,7 +148,7 @@ void *do_mem_map(size_t length) {
 
 // Unmaps the memory at "addr" of size "length" using the munmap syscall.
 // Returns: Nonzero on success, or -1 on fail.
-int do_mem_unmap(void *addr, size_t length) {
+int do_munmap(void *addr, size_t length) {
     if (length > 0) {
         int result =  munmap(addr, length);
         if (result != -1) {
@@ -123,79 +161,137 @@ int do_mem_unmap(void *addr, size_t length) {
 // Debug function, prints the given heap's properties
 void print_heap(HeapInfo *heap) {
     printf("Heap:\n");
-    printf("Size: %d\n", heap->size);
-    printf("SAddr: %d\n", heap->start_addr);
-    printf("DAddr: %d\n", heap->data_addr);
-    printf("FFree: %d\n", heap->first_free);
-    printf("End: %d\n", heap->end_addr);
-    printf("Max: %d\n", heap->max_addr);
+    printf("Size: %u\n", heap->size);
+    printf("SAddr: %u\n", heap->start_addr);
+    printf("DAddr: %u\n", heap->data_addr);
+    printf("FFree: %u\n", heap->first_free);
+    printf("End: %u\n", heap->end_addr);
+    printf("Max: %u\n", heap->max_addr);
 }
 
 // Debug function, prints the given BlockInfo's properties
 void print_block(BlockInfo *block) {
     printf("Block:\n");
-    printf("Size: %d\n", block->size);
-    printf("SAddr: %d\n", block->start_addr);
-    printf("DAddr: %d\n", block->data_addr);
-    printf("InUse: %d\n", block->in_use);
-    printf("PUsed: %d\n", block->prev_in_use);
-    printf("Next: %d\n", block->next);
+    printf("Size: %u\n", block->size);
+    printf("SAddr: %u\n", block->start_addr);
+    printf("DAddr: %u\n", block->data_addr);
+    printf("InUse: %u\n", block->in_use);
+    printf("PUsed: %u\n", block->prev_in_use);
+    printf("Next: %u\n", block->next);
 }
 
-// Inits a new heap with one free memory block.
-// Returns: A fully initialized heap of "mb_size" MBs, or NULL on error.
-void *new_heap(size_t mb_size) {
-    // Allocate the heap and first free mem block
-    size_t heap_size = (mb_size * (1 << mb_size));
-    HeapInfo *heap = do_mem_map(heap_size);
-    BlockInfo *first_free = do_mem_map(MIN_FREE_BLOCK_SZ);
+// Inits the global heap with one free memory block.
+// Returns: 0 on success, or -1 on error.
+int init_heap() {
+    // Allocate the heap and its single first free mem block
+    size_t heap_size = (START_HEAP_SZ * (1 << START_HEAP_SZ));
+    g_heap = do_mmap(heap_size);
+    BlockInfo *first_free = do_mmap(MIN_BLOCK_SZ);
 
-    if (!heap || !first_free)
-        return NULL;
+    if (!g_heap || !first_free)
+        return -1;
     
     // Init the first free block
     first_free->start_addr = (char*)first_free;
     first_free->data_addr = (char*)first_free + BLOCK_INFO_SZ;
-    first_free->size = MIN_FREE_BLOCK_SZ;
+    first_free->size = MIN_BLOCK_SZ;
     first_free->in_use = 0;
-    first_free->prev_in_use = 1;  // First block always sets prev = in use
+    first_free->prev_in_use = 1;  // First block always sets prev = true
     first_free->next = NULL;
 
-    // Init the heap
-    heap->first_free = first_free;  // First free block is one we just created
-    heap->size = heap_size;
-    heap->start_addr = (char*)heap;
-    heap->data_addr = (char*)heap + HEAP_INFO_SZ;
-    heap->end_addr = (char*)heap + MIN_FREE_BLOCK_SZ;
-    heap->max_addr = (char*)heap + heap_size;
+    // Init the g_heap
+    g_heap->first_free = first_free;  // First free block is one we just created
+    g_heap->size = heap_size;
+    g_heap->start_addr = (char*)g_heap;
+    g_heap->data_addr = (char*)g_heap + HEAP_INFO_SZ;
+    g_heap->end_addr = (char*)g_heap + MIN_BLOCK_SZ;
+    g_heap->max_addr = (char*)g_heap + heap_size;
     
-    return heap;
+    return 0;
 }
 
 // Frees the memory associated with the given heap
-void free_heap(HeapInfo *heap) {
+void free_heap() {
     printf("Freeing heap...\n");
-    print_heap(heap);
+    print_heap(g_heap);
 
-    while(heap->first_free) {
-        print_block(heap->first_free);
-        BlockInfo *next = heap->first_free->next;
-        do_mem_unmap(heap->first_free->start_addr, heap->first_free->size);
-        heap->first_free = next;
+    while(g_heap->first_free) {
+        print_block(g_heap->first_free);
+        BlockInfo *next = g_heap->first_free->next;
+        do_munmap(g_heap->first_free->start_addr, g_heap->first_free->size);
+        g_heap->first_free = next;
     }
 
-    do_mem_unmap(heap->start_addr, heap->size);
+    do_munmap(g_heap->start_addr, g_heap->size);
     printf("Done freeing heap.\n");
 }
-/* End Helpers ------------------------------------------------------------ */
+
+/* End Mem Helpers -------------------------------------------------------- */
+/* Begin malloc, calloc, realloc, free ------------------------------------ */
+
+// Allocates "size" bytes of memory in user space.
+// RETURNS: A ptr to the allocated memory on success, else returns NULL.
+void *__malloc_impl(size_t size) {
+    if (size > 0) {
+        if (!g_heap)                // If heap not yet initialized, init it
+            init_heap();
+
+
+        // Make room for block header and ensure min size
+        size += BLOCK_INFO_SZ;      
+        if (size < MIN_BLOCK_SZ)
+            size = MIN_BLOCK_SZ;
+        
+        // Ensure word-boundry alignment by rounding up
+        size = HEAP_ALIGN * ((size + HEAP_ALIGN - 1) / HEAP_ALIGN);
+
+        // Look for a block of this size in the heap's "free" list
+        BlockInfo* free_block = get_free_block(size);
+
+        // TODO: If none found, expand heap
+
+        // Remove the block from the free list
+
+
+        // BlockInfo *first_free = do_mmap(MIN_BLOCK_SZ);
+
+        // if (!heap || !first_free)
+        //     return NULL;
+        
+        // // Init the first free block
+        // first_free->start_addr = (char*)first_free;
+        // first_free->data_addr = (char*)first_free + BLOCK_INFO_SZ;
+        // first_free->size = MIN_BLOCK_SZ;
+        // first_free->in_use = 0;
+        // first_free->prev_in_use = 1;  // First block always sets prev = true
+        // first_free->next = NULL;
+        
+
+        void *result = do_mmap(size);
+
+        if (result != MAP_FAILED)
+            return result;
+    }
+    return NULL;
+}
+
+// Frees the memory space pointed to by ptr iff ptr != NULL
+void __free_impl(void *ptr) {
+    // if (ptr != NULL)
+    //     do_munmap(ptr);
+
+    // TODO: If the heap is empty, free it. It will re-init if needed.
+}
+
+/* End malloc, calloc, realloc, free -------------------------------------- */
+
+
 /* Begin Debug ------------------------------------------------------------ */
 
 int main(int argc, char **argv) {
-    // void *heap = new_heap(20);
-    if (!g_heap)
-        g_heap = new_heap(START_HEAP_SIZE);
-
-    free_heap(g_heap);
-
+    if (!g_heap) {
+        init_heap();
+        free_heap();
+    }
 
 }
